a921d70132800e644fa882f0b53ff507d0c0ab66
Study Fam <86848628+studyfam@users.noreply.github.com>
2022-11-28 23:07:03 +0000

    thing again

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Momentum',
+    version='1.0',
+    packages=["Babel", "cld3", "deep_translator", "discord.py", "emoji_country_fla", "flag", "Flask", "Pillow", "pycld3", "pymongo", "requests"
+],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description="mhm",
+)
